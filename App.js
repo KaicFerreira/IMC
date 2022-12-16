0900cf7aff28889f9a0063119f24b1c8d85eeb35
KaicFerreira <kaic.ferreira2016@outlook.com>
2022-12-16 11:10:20 -0300

    atualização

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+// import Titulo from './src/components/titulo';
+// import Principal from './src/components/principal';
+import Titulo from './src/components/titulo';
+import Principal from './src/components/principal';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Titulo />
+      <Principal />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizonte: 15,
+    backgroundColor: '#7FFFD4',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  fonteBlue: {
+    color: 'red' ,
+    fontWeight: 'bold',
+    fontSize: 30,
+  }
 });
